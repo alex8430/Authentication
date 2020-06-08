552e8f9c2f083fb2261e5ba7a42eaa69e1799179
alex8430 <pv24aug2001@gmail.com>
2020-06-08 13:14:31 +0530

    Setup File's and Database

---
+++ Secrets/app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose  = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/userDB",{ useNewUrlParser: true, useUnifiedTopology: true });
+
+userschema = {
+  email: String,
+  password: String
+};
+User  = new mongoose.model("User",userschema);
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.render("home");
+});
+
+app.route("/login")
+
+.get(function(req,res){
+  res.render("login");
+})
+
+.post(function(req,res){
+  User.findOne({email:req.body.username},function(err,foundUser){
+    if(err){
+      console.log(err);
+    }else{
+          if(foundUser.password === req.body.password){
+            console.log("Welcome user");
+            res.render("secrets");
+          }
+          else{
+            console.log("Your password or email was incorrect!")
+            res.redirect("/login");
+          }
+
+    }
+  });
+});
+
+
+
+app.route("/register")
+
+.get(function(req,res){
+  res.render("register");
+})
+
+.post(function(req,res){
+  const newUser =new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.render('secrets');
+    }
+  });
+
+});
+
+
+app.listen(3000,function(){
+  console.log("server start on port 3000");
+});
