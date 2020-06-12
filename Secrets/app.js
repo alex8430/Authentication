782e5adcacd14ea7db4a4cfd2a64f4d85a682f28
alex8430 <pv24aug2001@gmail.com>
2020-06-12 15:19:43 +0530

    Level-6 Security with  Google OAuth 2.0

--- Secrets/app.js
+++ Secrets/app.js
@@ -6,7 +6,10 @@ const mongoose  = require("mongoose");
 const session  =  require("express-session");
 const passport =  require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 const app = express();
+
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -28,22 +31,55 @@ mongoose.set('useCreateIndex', true);
 
 userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 User  = new mongoose.model("User",userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
+//[1]
+// -------------------------------------------------------------
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+// -----------------------------------------------------------------
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/",function(req,res){
   res.render("home");
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secret.
+    res.redirect('/secrets');
+  });
 
 // Login Route
 app.route("/login")
@@ -112,3 +148,25 @@ app.route("/register")
 app.listen(3000,function(){
   console.log("server start on port 3000");
 });
+
+
+
+/*[1]
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});              │
+                 │
+                 │
+                 └─────────────────┬──→ saved to session
+                                   │    req.session.passport.user = {id: '..'}
+                                   │
+                                   ↓
+passport.deserializeUser(function(id, done) {
+                   ┌───────────────┘
+                   │
+                   ↓
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });            └──────────────→ user object attaches to the request as req.user
+});
+*/
